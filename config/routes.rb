4548c47787c1dc4cd98799c96fec273e79d8f813
simonhlee97 <simonhlee97@hotmail.com>
2017-03-22 18:05:56 +0000

    added New Recipe Creation using TDD

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   
   root "pages#home"
   get 'pages/home', to: 'pages#home'
-  
-  get '/recipes/', to: 'recipes#index'
+  resources :recipes
+  #resources :recipes replaces the 4 lines below.
+  #get '/recipes/', to: 'recipes#index'
+  #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  #post'/recipes/', to: 'recipes#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
